cc644cf99b27960aca850c9a26f21d64d3258d20
llOriginaljuonll <129364650+llOriginaljuonll@users.noreply.github.com>
2024-11-02 21:42:44 +0700

    create Blog model

---
+++ apps/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Blog(models.Model):
+    """
+    Options:
+        auto_now อัปเดตข้อมูลอัตโนมัติ ทุกครั้งที่มีการแก้ไขข้อมูล
+    """
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    update_at = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"No.{self.id} {self.name}"
